22609346a9e1ea2d9f13f78848e6ff0bbbd193f2
Alexandre <alexandre.hacher@etu.univ-lehavre.fr>
2024-10-13 16:19:47 +0200

    Ajout des datas et lien avec le frontend, ajout des données sur les cartes photographes : image, ville, pays, tagline & prix

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,27 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+	const {name, id, city, country, tagline, price, portrait} = data;
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement('article');
+		const img = document.createElement('img');
+		img.setAttribute("src", picture)
+		const renderName = document.createElement('h2');
+		renderName.textContent = name;
+		const renderCityCountry = document.createElement('p');
+		renderCityCountry.textContent = city + ', ' + country;
+		const renderTagline = document.createElement('p');
+		renderTagline.textContent = tagline;
+		const renderPrice = document.createElement('p');
+		renderPrice.textContent = price + '€/jour';
+		article.appendChild(img);
+		article.appendChild(renderName);
+		article.appendChild(renderCityCountry);
+		article.appendChild(renderTagline);
+		article.appendChild(renderPrice);
+		return (article);
+	}
+
+	return {name, picture, getUserCardDOM}
 }
