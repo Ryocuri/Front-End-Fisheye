cd972df3d55d429a1b393469f3baf934fde07a48
Alexandre <alexandre.hacher@etu.univ-lehavre.fr>
2024-10-19 13:06:59 +0200

    Changement de la template photographers.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -20,7 +20,11 @@ async function getPhotographers() {
 }
 
 async function displayData(photographers) {
-	const photographersSection = document.querySelector(".photographer_section");
+	photographers.forEach((photographer) => {
+		const photographerModel = photographerTemplate(photographer);
+		const userCardDOM = photographerModel.getUserCardDOM();
+		document.querySelector(".photographer_section").appendChild(userCardDOM);
+	});
 
 	photographers.forEach((photographer) => {
 		const photographerModel = photographerTemplate(photographer);
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,18 +1,11 @@
 function photographerTemplate(data) {
 	const {name, id, city, country, tagline, price, portrait} = data;
-
-	const picture = `assets/photographers/${portrait}`;
-
-	function getUserCardDOM() {
-		let template = `<article class="photographer">` +
-			`<img src="assets/photographers/${portrait}" alt="${name}">` +
-			`<h2>${name}</h2>` +
-			`<p>${city}, ${country}</p>` +
-			`<p>${tagline}</p>` +
-			`<p>${price}€/jour</p>` +
-			`</article>`;
-		return (new DOMParser().parseFromString(template, 'text/html')).body.firstChild;
-	}
-
-	return {name, picture, getUserCardDOM}
+	const template = `<article class="photographer">
+				<img src="assets/photographers/${portrait}" alt="${name}">
+				<h2>${name}</h2>
+				<p>${city}, ${country}</p>
+				<p>${tagline}</p>
+				<p>${price}€/jour</p>
+			</article>`;
+	return (new DOMParser().parseFromString(template, 'text/html')).body.firstChild;
 }
