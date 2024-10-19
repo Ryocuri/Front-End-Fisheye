7072c72ffe3ad36e3f000a168c7991aa517155aa
Alexandre <alexandre.hacher@etu.univ-lehavre.fr>
2024-10-19 13:14:34 +0200

    Changement de la template photographers.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -29,8 +29,8 @@ async function displayData(photographers) {
 
 	photographers.forEach((photographer) => {
 		const photographerModel = photographerTemplate(photographer);
-		const userCardDOM = photographerModel.getUserCardDOM();
-		photographersSection.appendChild(userCardDOM);
+		//const userCardDOM = photographerModel.getUserCardDOM();
+		photographersSection.appendChild(photographerModel);
 	});
 }
 
